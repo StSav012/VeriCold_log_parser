176e83eb2f83405d88fe16c0953a948469395005
stsav012 <stsav012@gmail.com>
2021-01-25 20:19:51 +0300

    Fixes schemes for local files and Samba

--- parser.py
+++ parser.py
@@ -63,11 +63,11 @@ def _parse_with_numpy(filename: Union[str, Path]) -> Tuple[List[str], np.ndarray
     if np is None:
         raise ImportError('Module `numpy` is not loaded')
 
-    if isinstance(filename, str) and filename.startswith('file://'):
-        filename = filename[len('file://'):]
+    if isinstance(filename, str) and filename.startswith('file:'):
+        filename = filename[len('file:'):]
 
     with (filename.open('rb') if isinstance(filename, Path) else
-          __smb_opener.open(filename) if __smb_opener is not None and filename.startswith('smb://') else
+          __smb_opener.open(filename) if __smb_opener is not None and filename.startswith('smb:') else
           open(filename, 'rb')) as f_in:
         f_in.seek(0x1800 + 32)
         titles: List[str] = [f_in.read(32).strip(b'\0').decode('ascii') for _ in range(_CHANNELS_COUNT - 1)]
