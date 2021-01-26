cc2ab1feaeb1ddfd604010110a0f0d75ac03cd1a
stsav012 <stsav012@gmail.com>
2021-01-26 17:08:23 +0300

    Removed support for schemes and refactored
    The schemes should be handled elsewhere.

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
-from typing import List, Tuple, Union
+from typing import BinaryIO, List, Tuple, Union
 
 try:
     from typing import Final
@@ -18,37 +18,25 @@ except ImportError:
     np = None
     import struct
 
-try:
-    from smb.SMBHandler import SMBHandler
-except ImportError:
-    SMBHandler = None
-    __smb_opener = None
-else:
-    import urllib.request
-    __smb_opener = urllib.request.build_opener(SMBHandler)
-
 __all__ = ['parse']
 
 _CHANNELS_COUNT: Final[int] = 52
 
 
-def _parse_with_struct(filename: Union[str, Path]) -> Tuple[List[str], List[List[float]]]:
-    double_size: Final[int] = struct.calcsize('<d')
-    with (filename.open('rb') if isinstance(filename, Path) else
-          __smb_opener.open(filename) if __smb_opener is not None and filename.startswith('smb://') else
-          open(filename, 'rb')) as f_in:
-        f_in.seek(0x1800 + 32)
+def _parse_with_struct(filename: Union[str, Path, BinaryIO]) -> Tuple[List[str], List[List[float]]]:
+    def _parse(file_handle: BinaryIO):
+        file_handle.seek(0x1800 + 32)
         titles: List[str] = list(map(lambda s: s.strip(b'\0').decode('ascii'),
                                      struct.unpack_from('<' + '32s' * (_CHANNELS_COUNT - 1),
-                                                        f_in.read((_CHANNELS_COUNT - 1) * 32))))
-        f_in.seek(0x3000)
+                                                        file_handle.read((_CHANNELS_COUNT - 1) * 32))))
+        file_handle.seek(0x3000)
         data: List[List[float]] = [[] for _ in range(_CHANNELS_COUNT - 1)]
         while True:
-            data_size_data: bytes = f_in.read(double_size)
+            data_size_data: bytes = file_handle.read(double_size)
             if not data_size_data:
                 break
             data_size: int = int(struct.unpack_from('<d', data_size_data)[0]) - double_size
-            line_data: bytes = f_in.read(data_size)
+            line_data: bytes = file_handle.read(data_size)
             if len(line_data) != data_size:
                 raise IOError('Corrupted or incomplete data found')
             count: int = len(line_data) // double_size
@@ -58,27 +46,34 @@ def _parse_with_struct(filename: Union[str, Path]) -> Tuple[List[str], List[List
                 data[index].append(item)
         return titles, data
 
+    double_size: Final[int] = struct.calcsize('<d')
 
-def _parse_with_numpy(filename: Union[str, Path]) -> Tuple[List[str], np.ndarray]:
+    if isinstance(filename, BinaryIO):
+        return _parse(filename)
+    with (filename.open('rb') if isinstance(filename, Path) else open(filename, 'rb')) as f_in:
+        return _parse(f_in)
+
+
+def _parse_with_numpy(filename: Union[str, Path, BinaryIO]) -> Tuple[List[str], np.ndarray]:
     if np is None:
         raise ImportError('Module `numpy` is not loaded')
 
-    if isinstance(filename, str) and filename.startswith('file:'):
-        filename = filename[len('file:'):]
-
-    with (filename.open('rb') if isinstance(filename, Path) else
-          __smb_opener.open(filename) if __smb_opener is not None and filename.startswith('smb:') else
-          open(filename, 'rb')) as f_in:
-        f_in.seek(0x1800 + 32)
-        titles: List[str] = [f_in.read(32).strip(b'\0').decode('ascii') for _ in range(_CHANNELS_COUNT - 1)]
-        f_in.seek(0x3000)
+    def _parse(file_handle: BinaryIO):
+        file_handle.seek(0x1800 + 32)
+        titles: List[str] = [file_handle.read(32).strip(b'\0').decode('ascii') for _ in range(_CHANNELS_COUNT - 1)]
+        file_handle.seek(0x3000)
         dt = np.dtype(np.float64)
         dt = dt.newbyteorder('<')
-        data: np.ndarray = np.frombuffer(f_in.read(), dtype=dt)
+        data: np.ndarray = np.frombuffer(file_handle.read(), dtype=dt)
         if not (data.size / _CHANNELS_COUNT).is_integer():
             data = data[:-(data.size % _CHANNELS_COUNT)]
             # raise RuntimeError(f'Do not know how to process {data.size} numbers')
         return titles, data.reshape((_CHANNELS_COUNT, -1), order='F')[1:]
+
+    if isinstance(filename, BinaryIO):
+        return _parse(filename)
+    with (filename.open('rb') if isinstance(filename, Path) else open(filename, 'rb')) as f_in:
+        return _parse(f_in)
 
 
 if np is None:
