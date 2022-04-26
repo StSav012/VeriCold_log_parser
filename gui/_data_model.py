95d2118edce7cf9b3a26626adbcb1aa57f881e2c
stsav012 <stsav012@gmail.com>
2022-04-26 17:41:31 +0300

    Used the power of the LRU cache to make the formatting faster

--- gui/_data_model.py
+++ gui/_data_model.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
+import functools
 from datetime import datetime
 from typing import Final, List, Optional, Union, cast
 
 import numpy as np
 from numpy.typing import NDArray
 from pyqtgraph.Qt import QtCore
+
+__all__ = ['DataModel']
+
+
+@functools.lru_cache(maxsize=128, typed=True)
+def format_float(value: np.float64, precision: int = 12) -> str:
+    return f'{value:.{precision}f}'.rstrip('0').rstrip('.')
 
 
 class DataModel(QtCore.QAbstractTableModel):
@@ -40,12 +48,12 @@ class DataModel(QtCore.QAbstractTableModel):
         if self._header[column].endswith(('(s)', '(sec)', '(secs)')):
             return datetime.fromtimestamp(value).isoformat()
         if self._header[column].endswith('(K)'):
-            return str(np.around(value, decimals=3))
+            return format_float(value)
         if self._header[column].endswith('(Bar)'):
-            return cast(str, np.format_float_positional(value, precision=3 + int(-np.log10(np.abs(value)))))
+            return format_float(value, precision=3 + int(-np.log10(np.abs(value))))
         if value.is_integer():
             return f'{value:.0f}'
-        return str(np.around(value, decimals=12))
+        return format_float(value)
 
     def data(self, index: QtCore.QModelIndex,
              role: QtCore.Qt.ItemDataRole = QtCore.Qt.ItemDataRole.DisplayRole) -> Optional[str]:
@@ -91,9 +99,6 @@ class DataModel(QtCore.QAbstractTableModel):
         return bool(self._data.shape[1] > self._rows_loaded)
 
     def fetchMore(self, index: QtCore.QModelIndex = QtCore.QModelIndex()) -> None:
-        # FIXME: if the 0th column is hidden, no data gets fetched despite it is available according to `canFetchMore`
-        #  For now, the only solution is to load more than one screen can display. If the table is scrolled, data loads.
-        # https://sateeshkumarb.wordpress.com/2012/04/01/paginated-display-of-table-data-in-pyqt/
         reminder: int = self._data.shape[1] - self._rows_loaded
         items_to_fetch: int = min(reminder, self.ROW_BATCH_COUNT)
         self.beginInsertRows(QtCore.QModelIndex(), self._rows_loaded, self._rows_loaded + items_to_fetch - 1)
